3dafa31b0bb30128a5e2cc46db4cd2b1a0ea1796
Barbar Huber <bhuber.mmt-b2010@fh-salzburg.ac.at>
2014-06-04 13:32:40 +0200

    change contentwizard for easier handling

--- typo3conf/ext/content/class.contentwizard.php
+++ typo3conf/ext/content/class.contentwizard.php
@@ -16,14 +16,68 @@ class user_contentWizard implements cms_newContentElementWizardsHook{
 //				),
 				'params' => '&defVals[tt_content][CType]=content_szene'
 			),
-			'content_menu' => array(
+			'content_path' => array(
 				'icon'=>'gfx/c_wiz/sitemap.gif',
-				'title'=>'Menü',
-				'description'=>'Inhalts-Element Menüansicht aller Szenen',
+				'title'=>'Pfad',
+				'description'=>'Inhalts-Element Pfad',
 //				'tt_content_defValues' => array(
 //					'CType' => 'content_pi1',
 //				),
-				'params' => '&defVals[tt_content][list_type]=content_menu'
+				'params' => '&defVals[tt_content][list_type]=content_path'
+			),
+			'content_overlay' => array(
+				'icon'=>'gfx/c_wiz/div.gif',
+				'title'=>'Overlay',
+				'description'=>'Inhalts-Element Overlay',
+//				'tt_content_defValues' => array(
+//					'CType' => 'content_pi1',
+//				),
+				'params' => '&defVals[tt_content][list_type]=content_overlay'
+			),
+			'content_overlayBlack' => array(
+				'icon'=>'gfx/c_wiz/div.gif',
+				'title'=>'Overlay Schwarz',
+				'description'=>'Inhalts-Element Overlay Schwarz',
+//				'tt_content_defValues' => array(
+//					'CType' => 'content_pi1',
+//				),
+				'params' => '&defVals[tt_content][list_type]=content_overlayBlack'
+			),
+			'content_accordion' => array(
+				'icon'=>'gfx/c_wiz/bullet_list.gif',
+				'title'=>'Akkordion',
+				'description'=>'Inhalts-Element Akkordion',
+//				'tt_content_defValues' => array(
+//					'CType' => 'content_pi1',
+//				),
+				'params' => '&defVals[tt_content][CType]=content_accordion'
+			),
+			'content_text' => array(
+				'icon'=>'gfx/c_wiz/regular_text.gif',
+				'title'=>'Textelement',
+				'description'=>'Inhalts-Element Text',
+//				'tt_content_defValues' => array(
+//					'CType' => 'content_pi1',
+//				),
+				'params' => '&defVals[tt_content][CType]=content_text'
+			),
+			'content_video_loop' => array(
+				'icon'=>'gfx/c_wiz/div.gif',
+				'title'=>'Video Overlay',
+				'description'=>'Inhalts-Element Video Overlay',
+//				'tt_content_defValues' => array(
+//					'CType' => 'content_pi1',
+//				),
+				'params' => '&defVals[tt_content][CType]=content_video_loop'
+			),
+			'content_settings' => array(
+				'icon'=>'gfx/c_wiz/table.gif',
+				'title'=>'Einstellungen',
+				'description'=>'Inhalts-Element Einstellungen',
+//				'tt_content_defValues' => array(
+//					'CType' => 'content_pi1',
+//				),
+				'params' => '&defVals[tt_content][CType]=content_settings'
 			),
 			'plugins_1' => array(
 			'icon'=>'gfx/c_wiz/user_defined.gif',
